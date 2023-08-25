137ba4852ce14a5a8d3e165eb8c5c9d070b362ee
Jaqueline <jaquelinenacarate@gmail.com>
2023-08-25 21:52:36 +0000

    Try to create a store & display error

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,20 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+# Ask user for store name
+print "Enter a store name: "
+store_name = gets.chomp
+
+# Attempt to create a store
+new_store = Store.new(name: store_name)
+
+
+if new_store.save
+  puts "Store '#{store_name}' created successfully!"
+else
+  puts "Failed to create the store due to the following errors:"
+  new_store.errors.full_messages.each do |error|
+    puts error
+  end
+end
