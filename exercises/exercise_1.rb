e63308800ddfa598c858d6435dae8ad453b40502
Jaqueline <jaquelinenacarate@gmail.com>
2023-08-25 20:22:38 +0000

    Use string interpolation

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -9,4 +9,4 @@ stores = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: tru
 stores = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
 stores = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
-puts "Number of stores:" Store.count
+puts "Number of stores: #{Store.count}"
