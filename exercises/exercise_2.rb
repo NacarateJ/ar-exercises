014470ff8bbcc494f2967210fe415944bb2f27ad
Jaqueline <jaquelinenacarate@gmail.com>
2023-08-25 20:33:08 +0000

    Update the first store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -6,4 +6,10 @@ puts "----------"
 
 # Your code goes here ...
 
+@store1 = Store.find_by(id: 1)
 
+@store2 = Store.find_by(id: 2)
+
+@store1.update(name: "BurnabyUpdated")
+
+puts "First store's name updated to: #{@store1.name}"
