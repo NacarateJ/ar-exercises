1891374dac8b4cb91dad09fe79173f3778e38c80
Jaqueline <jaquelinenacarate@gmail.com>
2023-08-25 21:51:29 +0000

    Validations for both models

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,15 @@
 class Store < ActiveRecord::Base
   has_many :employees
+
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+  validate :must_carry_apparel
+
+  private
+
+  def must_carry_apparel
+    if mens_apparel.blank? && womens_apparel.blank?
+      errors.add(:base, "Must carry at least one type of apparel")
+    end
+  end
 end
