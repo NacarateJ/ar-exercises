c2d4c3b5cc8ef781a4e7fa28d9a95d43dabbbf7b
Jaqueline <jaquelinenacarate@gmail.com>
2023-08-26 00:13:55 +0000

    Add callback to check for employees before destry

--- lib/store.rb
+++ lib/store.rb
@@ -5,11 +5,20 @@ class Store < ActiveRecord::Base
   validates :annual_revenue, numericality: { greater_than_or_equal_to: 0, only_integer: true }
   validate :must_carry_apparel
 
+  before_destroy :check_for_employees
+
   private
 
   def must_carry_apparel
     if mens_apparel.blank? && womens_apparel.blank?
       errors.add(:base, "Must carry at least one type of apparel")
+    end
+  end
+
+  def check_for_employees
+    if employees.any?
+      errors.add(:base, "Cannot destroy a store with employees")
+      throw(:abort)
     end
   end
 end
